03e3107c6269de90a4433cc06f1d3d5c18c7771e
Rusty Bailey <rustylbailey@gmail.com>
2012-10-17 11:39:16 -0300

    Update pe1.js
    Fixed some syntax.

--- pe1.js
+++ pe1.js
@@ -1,13 +1,13 @@
 //Add all the natural numbers below one thousand that are multiples of 3 or 5.
 
-var multiplesSum = function(n){
+var multiplesSum = function(n) {
     var sumThreeFive = 0;
     for (var i = 1; i < n; i++) {
         if (i % 3 === 0 || i % 5 === 0) {
-            sumThreeFive += i
-        };
-    };
-    return sumThreeFive    
-}
+            sumThreeFive += i;
+        }
+    }
+    return sumThreeFive;
+};
 
 console.log(multiplesSum(1000));
